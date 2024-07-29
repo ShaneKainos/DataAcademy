2ee2522ecd89773677e5d384b34e527a0f09ff67
nhsbsa-shara <shane.rafferty@nhsbsa.nhs.uk>
2024-07-29 15:50:23 +0100

    Additions made to math functions

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from math_functions import add
+from math_functions import subtract
 import random
 
 app = FastAPI()
@@ -7,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World", "value": str(add(random.randint(0, 10), random.randint(11, 30)))}
+
+@app.get("/subtract")
+async def root():
+    num1 = random.randint(10, 20)
+    num2 = random.randint(0, 10)
+    return {"message": "Subtracting " + num1 + " - " + num2, "value": str(subtract(num1 - num2))}
