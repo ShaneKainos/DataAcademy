e0c3f141e4ea0506f443866f971120861b9d4b28
nhsbsa-shara <shane.rafferty@nhsbsa.nhs.uk>
2024-07-29 16:09:30 +0100

    Touch ups

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from math_functions import subtract
+from math_functions import add, subtract
 import random
 
 app = FastAPI()
@@ -9,7 +9,7 @@ async def root():
     return {"message": "Hello World", "value": str(add(random.randint(0, 10), random.randint(11, 30)))}
 
 @app.get("/subtract")
-async def root():
+async def subtract_route():
     num1 = random.randint(10, 20)
     num2 = random.randint(0, 10)
-    return {"message": "Subtracting " + num1 + " - " + num2, "value": str(subtract(num1 - num2))}
+    return {"message": f"Subtracting {num1} - {num2}", "value": str(subtract(num1, num2))}
